7e6cce802bd1d6c2c71c306732335a9580bc392d
Suhas Dinesh <suhassamarth@gmail.com>
2021-05-10 16:24:50 +0530

    intial

--- app/Models/User.php
+++ app/Models/User.php
@@ -20,8 +20,10 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'phone',
     ];
 
+    protected $table = 'users';
     /**
      * The attributes that should be hidden for arrays.
      *
--- routes/backpack/custom.php
+++ routes/backpack/custom.php
@@ -6,12 +6,16 @@
 // This route file is loaded automatically by Backpack\Base.
 // Routes you generate using Backpack\Generators will be placed here.
 
+
 Route::group([
     'prefix'     => config('backpack.base.route_prefix', 'admin'),
     'middleware' => array_merge(
         (array) config('backpack.base.web_middleware', 'web'),
         (array) config('backpack.base.middleware_key', 'admin')
     ),
-    'namespace'  => 'App\Http\Controllers\Admin',
-], function () { // custom admin routes
+    'namespace'  => 'App\Http\Controllers',
+], function () { 
+    Route::get('/admin/register','RegisterController@showRegistrationForm')->name('backpack.auth.register');
+    Route::post('/admin/register', 'RegisterController@register');
+    // custom admin routes
 }); // this should be the absolute last line of this file
